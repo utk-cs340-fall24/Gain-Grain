638777054e11cd1a35884ca1b86f1008d856d021
V1w35h <vgupta8@vols.utk.edu>
2024-09-23 20:34:42 -0400

    adding some changes to new pages

--- gain-grain/src/app/dashboard/Excercise/page.js
+++ gain-grain/src/app/dashboard/Excercise/page.js
@@ -3,11 +3,8 @@ import Link from 'next/link';
 export default function Exercise() {
   
   return (
-    <main className="flex min-h-screen flex-col p-6">
-      <div className="flex h-20 shrink-0 items-end rounded-lg bg-orange-500 p-4 md:h-52">
+      <div>
         <h1>Let's workout! </h1>
       </div>
-    </main>
-    
   );
 }
--- gain-grain/src/app/dashboard/Nutrition/page.js
+++ gain-grain/src/app/dashboard/Nutrition/page.js
@@ -1,14 +1,12 @@
 import Link from 'next/link';
 
-export default function Calender() {
+export default function Nutrition() {
   
   return (
     <main className="flex min-h-screen flex-col p-6">
       <div className="flex h-20 shrink-0 items-end rounded-lg bg-orange-500 p-4 md:h-52">
-        <h1>Nutrition & Workout Calender: </h1>
+        <h1>Let's cook a delicious meal! </h1>
       </div>
-      <h1>Plan a meal</h1>
-      <h1>Plan a Workout</h1>
     </main>
   );
 }
