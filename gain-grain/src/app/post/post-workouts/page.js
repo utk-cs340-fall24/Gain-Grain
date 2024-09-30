e3e1033fc2d65cced7b6584fb186a2cb89d24a0a
Peyton Pridemore <ppridemo@vols.utk.edu>
2024-09-30 12:21:01 -0400

    Added the subpages for each type of post, and linked them

---
+++ gain-grain/src/app/post/post-workouts/page.js
@@ -0,0 +1,7 @@
+export default function PostWorkouts() {
+    return (
+        <div className="min-h-screen flex items-center justify-center bg-gray-100 p-6">
+            <h1 className="text-3xl font-bold">Post Workouts</h1>
+        </div>
+    );
+}
