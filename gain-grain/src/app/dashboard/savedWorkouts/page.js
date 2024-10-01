0707b13984890b5c19ed677407552b43f975263f
V1w35h <vgupta8@vols.utk.edu>
2024-10-01 08:04:49 -0400

    small commit

--- gain-grain/src/app/dashboard/savedWorkouts/page.js
+++ gain-grain/src/app/dashboard/savedWorkouts/page.js
@@ -10,7 +10,7 @@ export default function Exercise() {
   
   return (
       <div className="flex h-20 shrink-0 items-end rounded-lg bg-orange-500 p-4 md:h-52">
-        <h1 className="text-white text-xl">Let's workout! </h1>
+        <h1 className="text-white text-xl">Saved Exercises </h1>
         
       </div>
   );
