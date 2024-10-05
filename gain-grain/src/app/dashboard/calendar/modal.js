adecac5d2ee6bbeb3964599dde32f147de9b67ea
brodiekovach <bkovach@vols.utk.edu>
2024-10-04 21:29:44 -0400

    added close button to adding in calendar

--- gain-grain/src/app/dashboard/calendar/modal.js
+++ gain-grain/src/app/dashboard/calendar/modal.js
@@ -4,17 +4,13 @@ import { FaTimes } from 'react-icons/fa';
 import './modal.css'; // Make sure to create and style this CSS file
 
 const Modal = ({ show, onClose, children }) => {
-    if (!show) {
-        return null;
-    }
+    if (!show) return null; // Don't render the modal if `show` is false
 
     return (
         <div className="modal-overlay">
             <div className="modal-content">
-                <button className="close-btn" onClick={onClose}>
-                    <FaTimes />
-                </button>
-                {children}
+                <FaTimes className="modal-close" onClick={onClose}/> {/* Close button */}
+                {children} {/* Render the children passed to Modal */}
             </div>
         </div>
     );
