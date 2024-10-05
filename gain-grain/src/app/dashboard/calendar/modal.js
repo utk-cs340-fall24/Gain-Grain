02139d5382bac89d5942e7d5e3cc5f7195ae1e32
brodiekovach <bkovach@vols.utk.edu>
2024-10-04 23:27:46 -0400

    modal animation

--- gain-grain/src/app/dashboard/calendar/modal.js
+++ gain-grain/src/app/dashboard/calendar/modal.js
@@ -1,16 +1,27 @@
-// Modal.js
-import React from 'react';
+import React, { useState } from 'react';
 import { FaTimes } from 'react-icons/fa';
-import './modal.css'; // Make sure to create and style this CSS file
+import './modal.css'; // Ensure to style this CSS file
 
 const Modal = ({ show, onClose, children }) => {
-    if (!show) return null; // Don't render the modal if `show` is false
+    const [isClosing, setIsClosing] = useState(false);
+
+    const handleClose = () => {
+        setIsClosing(true); // Start the closing animation
+        setTimeout(() => {
+            onClose(); // Close the modal after animation finishes
+            setIsClosing(false); // Reset closing state
+        }, 250); // Match the timeout to animation duration (0.5s)
+    };
+
+    if (!show && !isClosing) return null; // Don't render if modal is not shown and not in the process of closing
 
     return (
         <div className="modal-overlay">
-            <div className="modal-content">
-                <FaTimes className="modal-close" onClick={onClose}/> {/* Close button */}
-                {children} {/* Render the children passed to Modal */}
+            <div className={`modal-content ${isClosing ? 'modal-close-animation' : 'modal-open-animation'}`}>
+                <button className="modal-close" onClick={handleClose}>
+                    <FaTimes />
+                </button>
+                {children}
             </div>
         </div>
     );
