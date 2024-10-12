5afc6b1df3593870f19913c47b950b7482acf8cd
teisenba <teisenba@vols.utk.edu>
2024-10-12 11:37:25 -0400

    added some documentation and changed hamburger menu buttons up gh-28

--- gain-grain/src/components/Navbar.js
+++ gain-grain/src/components/Navbar.js
@@ -34,11 +34,13 @@ export default function Navbar() {
                 />
             </div>
             <div className={styles.buttonContainer}>
+                {/* calendar button */}
                 <Link href="/dashboard/calendar">
                     <button className="bg-orange-500 text-white font-semibold py-2 px-4 rounded-lg hover:bg-orange-600 transition-all">
                         Calendar
                     </button>
                 </Link>
+                {/* post button */}
                 <Link href="/post">
                     <button className="bg-orange-500 text-white font-semibold py-2 px-4 rounded-lg hover:bg-orange-600 transition-all">
                         Post
@@ -51,29 +53,33 @@ export default function Navbar() {
                 {showDropdown ?
                     <div className={styles.hamburgerMenu}>
                         <div className="flex flex-col space-y-1 my-1">
-                            <Link href="/dashboard/savedMeals">
-                                <button className="w-full bg-orange-500 text-white font-semibold py-2 px-4 rounded-lg hover:bg-orange-500 hover:text-white transition-all">
-                                    Nutrition
-                                </button>
-                            </Link>
-                            <Link href="/dashboard/savedWorkouts">
-                                <button className="w-full bg-orange-500 text-white font-semibold py-2 px-4 rounded-lg hover:bg-orange-500 hover:text-white transition-all">
-                                    Exercise
-                                </button>
-                            </Link>
-                            {/* Combined login/register button */}
                             <Link href="/login">
-                                <button className="w-full bg-orange-500 text-white font-semibold py-2 px-4 rounded-lg hover:bg-orange-500 hover:text-white transition-all">
+                                {/* combined login/register button */}
+                                <button className="w-full bg-orange-500 text-white font-semibold py-2 px-4 rounded-lg hover:bg-orange-600 transition-all">
                                     Login/Register
                                 </button>
                             </Link>
                             <Link href="/profile">
-                                <button className="w-full bg-orange-500 text-white font-semibold py-2 px-4 rounded-lg hover:bg-orange-500 hover:text-white transition-all">
+                                <button className="w-full bg-orange-500 text-white font-semibold py-2 px-4 rounded-lg hover:bg-orange-600 transition-all">
                                     Profile
+                                </button>
+                            </Link>
+                            <Link href="/dashboard/savedMeals">
+                                {/* nutrition button */}
+                                <button className="w-full bg-orange-500 text-white font-semibold py-2 px-4 rounded-lg hover:bg-orange-600 transition-all">
+                                    Nutrition
+                                </button>
+                            </Link>
+                            
+                            <Link href="/dashboard/savedWorkouts">
+                                {/* exercise button*/}
+                                <button className="w-full bg-orange-500 text-white font-semibold py-2 px-4 rounded-lg hover:bg-orange-600 transition-all">
+                                    Exercise
                                 </button>
                             </Link>
                         </div>
                     </div>
+                    // if not clicked, render nothing
                     :<></>
                 }
             </div>
